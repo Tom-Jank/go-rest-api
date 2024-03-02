37405058f78b85514a44cd5a58086dad339489b3
Tom-Jank <tom.jank4@gmail.com>
2024-03-02 13:52:17 +0100

    create separate handlers

--- api/routes/routes.go
+++ api/routes/routes.go
@@ -5,15 +5,19 @@ import "github.com/gin-gonic/gin"
 func HelloRoute(route *gin.Engine) {
 	hello := route.Group("/greeting")
 	{
-		hello.GET("/hello", func(c *gin.Context) {
-			c.JSON(200, gin.H{
-				"message": "hello",
-			})
-		})
-		hello.GET("/goodmorning", func(c *gin.Context) {
-			c.JSON(200, gin.H{
-				"message": "goodmorning",
-			})
-		})
+		hello.GET("/hello", helloResponseHandler)
+		hello.GET("/goodmorning", goodmorningResponseHandler)
 	}
+}
+
+func helloResponseHandler(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "hello",
+	})
+}
+
+func goodmorningResponseHandler(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "goodmorning",
+	})
 }
